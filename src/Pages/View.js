58053c33994265bd3c1428448161074f347a0997
Samuel Vandamme <samuel@sava.be>
2020-05-29 14:29:14 +0100

    Updated View component to new data format

--- src/Pages/View.js
+++ src/Pages/View.js
@@ -14,7 +14,7 @@ class View extends React.Component {
         super(props);
 
         this.state = {
-            'dashboard': data.dashboards.find(element => element.name === props.match.params.handle),
+            'quickstart': data.quickstarts.find(element => element.id === props.match.params.handle),
             'visible': 0,
             'modalVisible': false,
             'accountId': '',
@@ -55,8 +55,9 @@ class View extends React.Component {
     }
 
     getDashboard(file, callback) {
+        console.log(file);
         this.getAccountId(() => {
-            fetch('./data/' + file.location).then(response => response.json()).then((json) => {
+            fetch('./data/' + file).then(response => response.json()).then((json) => {
                 json.dashboard_account_id = +this.state.accountId;
                 callback(JSON.stringify(json));
             });
@@ -84,7 +85,7 @@ class View extends React.Component {
                 <div className="container" id="root">
                     <div className="row py-4">
                         <div className="col-8">
-                            <h2>{ this.state.dashboard.config.name }</h2>
+                            <h2>{ this.state.quickstart.name }</h2>
                         </div>
                         <div className="col-4 text-right">
                             <Link className="btn btn-default" to={"/"}>
@@ -94,64 +95,48 @@ class View extends React.Component {
                     </div>
                     <div className="row">
                         <div className="col-8">
-                            <p><b>Created by:</b> { this.state.dashboard.config.author }</p>
-                            <Datasource sources={this.state.dashboard.sources} />
+                            <p><b>Created by:</b> { this.state.quickstart.authors.join(', ') }</p>
+                            <Datasource sources={this.state.quickstart.sources} />
                             <h5 className="pt-4">Installation instructions</h5>
                             <p>This dashboard requires the following New Relic products:</p>
-                            <InstallationInstructions sources={this.state.dashboard.sources} />
+                            <InstallationInstructions sources={this.state.quickstart.sources} />
                         </div>
                         <div className="col-4 text-right">
-                            <a class="btn btn-danger" href={"https://github.com/newrelic-experimental/quickstarts/issues/new?labels=bug&title=Problem%20with%20" + this.state.dashboard.config.name}>Report a problem</a>
+                            <a className="btn btn-danger" href={"https://github.com/newrelic-experimental/quickstarts/issues/new?labels=bug&title=Problem%20with%20" + this.state.quickstart.id}>Report a problem</a>
                         </div>
-                    </div>
-                    <div className="row py-4">
-                        <h3>Dashboards</h3>
                     </div>
                     <div className="row">
                         <div className="col-12">
-                            <ul className="nav nav-tabs">
-                                {this.state.dashboard.files.map((file, i) => {
-                                    let selected = '';
-                                    if (this.state.visible === i) selected = 'active';
-                                    return (
-                                        <li className="nav-item" key={file.name}>
-                                            <button className={"nav-link btn " + selected} onClick={(event) => { this.setState({visible: i}) }}>
-                                                {file.name}
-                                            </button>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        </div>
-                        {this.state.dashboard.files.map((file, i) => {
-                            if (this.state.visible !== i) return ( <span key={file.name}></span> );
-                            return (
-                                <div className="row px-4 py-2 dashboard-info bg-light" key={file.name}>
-                                    <div className="col-12 py-3 text-right">
-                                        <div className="btn-group" role="group" aria-label="Basic example">
-                                            <button className="btn btn-primary">Import to New Relic (TODO)</button>
-                                            <button className="btn btn-info" onClick={(event) => { this.copy(file) }}>Copy to clipboard</button>
+                            <h2>Dashboards</h2>
+
+                            {this.state.quickstart.dashboards.map((dashboard) => {
+                                return (
+                                    <div key={dashboard.filename} className="row px-4 py-2">
+                                        <div className="col-8">
+                                            <h3>{dashboard.name}</h3>
+                                        </div>
+                                        <div className="col-4 py-3 text-right">
+                                            <div className="btn-group" role="group" aria-label="Basic example">
+                                                {/* <button className="btn btn-primary">Import to New Relic (TODO)</button> */}
+                                                <button className="btn btn-info" onClick={(event) => { this.copy('./' + this.state.quickstart.id + '/dashboards/' + dashboard.filename) }}>Copy to clipboard</button>
+                                            </div>
+                                        </div>
+                                        <div className="col-12">
+                                            {dashboard.screenshots.map((screenshot) => {
+                                                return (
+                                                    <img key={screenshot} src={ "data/" + this.state.quickstart.id + "/dashboards/" + screenshot} className="card-img-top" alt="..." />
+                                                );
+                                            })}
                                         </div>
                                     </div>
-                                    <div className="col-12">
-                                        <img src={ "data/" + this.state.dashboard.name + "/" + this.state.dashboard.screenshots[i]} className="card-img-top" alt="..." />
-                                    </div>
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div className="row py-4">
-                        <div className="col-12">
-                            <h3>Alerts</h3>
-                        </div>
-                        <div className="col-12">
-                            <p>Work in progress: Example alerts the customer could set-up</p>
+                                )
+                            })}
                         </div>
                     </div>
                 </div>
 
                 {this.state.modalVisible&&
-                    <div className="modal fade show" tabIndex="-1" role="dialog" style={{"display": "block", "background-color": "rgba(150, 150, 150, 0.50)"}}>
+                    <div className="modal fade show" tabIndex="-1" role="dialog" style={{"display": "block", "backgroundColor": "rgba(150, 150, 150, 0.50)"}}>
                         <div className="modal-dialog" role="document">
                             <div className="modal-content">
                                 <div className="modal-header">
