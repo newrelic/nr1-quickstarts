6a1c8d6fd26526765d7b770ea9777f96db525571
Samuel Vandamme <samuel@sava.be>
2020-12-15 12:01:23 +0100

    Removed bootstrap from App.js

--- packages/nerdlet/nerdlets/nerdlet-nerdlet/src/App.js
+++ packages/nerdlet/nerdlets/nerdlet-nerdlet/src/App.js
@@ -8,7 +8,11 @@ import Home from './Pages/Home';
 import View from './Pages/View';
 import ScrollToTop from './Helpers/ScrollToTop';
 import Tools from './Pages/Tools';
-import { Spinner } from 'nr1';
+import {
+    Grid,
+    GridItem,
+    Spinner
+} from 'nr1';
 
 class App extends React.Component {
 
@@ -35,20 +39,17 @@ class App extends React.Component {
     render() {
         if (this.state.loading) {
             return (
-                <div className="container">
-                    <div className="row">
-                        <div className="col-12 text-center loading">
-                            <p>Loading</p>
-                            <Spinner />
-                        </div>
-                    </div>
-                </div>
+                <Grid>
+                    <GridItem columnSpan={12} className="text-center loading">
+                        <Spinner />
+                    </GridItem>
+                </Grid>
             )
         }
 
         return (
             <HashRouter>
-                <main role="main" id="root">
+                <main role="main">
                     <Switch>
                         <Route path="/view/:handle" render={(props) => <View data={this.state.data} {...props} /> } />
                         <Route path="/tools" render={(props) => <Tools data={this.state.data} {...props} /> } />
