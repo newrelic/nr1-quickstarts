85c850e5f9c08c51097a2024e573dbee3372f46c
Samuel Vandamme <samuel@sava.be>
2021-01-06 10:27:10 +0100

    Added errors to create mutation

--- packages/nerdlet/nerdlets/nerdlet-nerdlet/src/Pages/Modals/ExportModal.js
+++ packages/nerdlet/nerdlets/nerdlet-nerdlet/src/Pages/Modals/ExportModal.js
@@ -101,6 +101,10 @@ class ExportModal extends React.Component {
     createMutation = `
     mutation($accountId: Int!, $dashboard: DashboardInput!) {
         dashboardCreate(accountId: $accountId, dashboard: $dashboard) {
+            errors {
+                description
+                type
+            }
             entityResult {
                 guid
             }
